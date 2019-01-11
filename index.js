7d42c8bb7df530700b66a9dd7d4759cd95b3c0f6
Alvin Rao <alvinrao7@gmail.com>
2019-01-11 19:18:11 +0000

    adding file with chalk styling

---
+++ index.js
@@ -0,0 +1,6 @@
+var chalk = require("chalk");
+
+var message = chalk.bold("Hello ") + chalk.italic("today ") + chalk.red("we will be learning ") + chalk.yellow("what npm is ");
+
+
+console.log(message);
